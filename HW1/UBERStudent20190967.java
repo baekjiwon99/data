3a7bc8d934da5a18be02e06f3f1ee38536e60f9e
baekjiwon99 <102844803+baekjiwon99@users.noreply.github.com>
2023-05-16 17:47:35 +0900

    Update UBERStudent20190967.java

--- HW1/UBERStudent20190967.java
+++ HW1/UBERStudent20190967.java
@@ -1,19 +1,20 @@
 import java.io.IOException;
 import java.lang.InterruptedException;
 import java.util.*;
+import java.time.*;
 import org.apache.hadoop.fs.*;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.conf.Configuration;
-import java.time.*;
 
 public class UBERStudent20190967 {
+	
 	public static String getDay(String d) throws IOException, InterruptedException {
 		StringTokenizer itr = new StringTokenizer(d, "/");
 		int month = Integer.parseInt(itr.nextToken().trim());
@@ -50,6 +51,7 @@ public class UBERStudent20190967 {
   		return rslt;
 	}
 	public static class UberMapper extends Mapper<Object, Text, Text, Text>{
+
 		private Text outputKey = new Text();
 		private Text outputVal = new Text();
 		
@@ -57,8 +59,8 @@ public class UBERStudent20190967 {
 			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
 			
 			String region = itr.nextToken().trim();
-			String date = itr.nextToken().trim();
 			String vehicles = itr.nextToken().trim();
+			String date = itr.nextToken().trim();
 			String trips = itr.nextToken().trim();
 			
 			outputKey.set(region + "," + getDay(date));
@@ -68,20 +70,21 @@ public class UBERStudent20190967 {
 		}
 	}
 	public static class UberReducer extends Reducer<Text, Text, Text, Text>{
+		
 		private Text outputVal = new Text();
 		
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
-			int sumOfTrips = 0;
-			int sumOfVehicles = 0;
+			int sumTrips = 0;
+			int sumVehicles = 0;
 			
 			for(Text v : values) {
 				StringTokenizer itr = new StringTokenizer(v.toString(), ",");
 				int trips = Integer.parseInt(itr.nextToken().trim());
 				int vehicles = Integer.parseInt(itr.nextToken().trim());
-				sumOfTrips += trips;
-				sumOfVehicles += vehicles;
+				sumTrips += trips;
+				sumVehicles += vehicles;
 			}
-			outputVal.set(sumOfTrips + "," + sumOfVehicles);
+			outputVal.set(sumTrips + "," + sumVehicles);
 			context.write(key, outputVal);
 		}
 	
@@ -90,19 +93,23 @@ public class UBERStudent20190967 {
 	public static void main(String[] args) throws Exception{
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		
 		if(otherArgs.length != 2){
 			System.err.println("Usage: Uber <in> <out>"); System.exit(2);
 		}
 
 		Job job = new Job(conf, "UBERStudent20190967");
+		
 		job.setJarByClass(UBERStudent20190967.class);
 		job.setMapperClass(UberMapper.class);
 		job.setReducerClass(UberReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
+		
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 		FileSystem.get(job.getConfiguration()).delete(new Path(otherArgs[1]), true);
+		
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
