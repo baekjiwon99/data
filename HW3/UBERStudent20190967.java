0071879a4e619599b18063d34d5edbd6158c3560
baekjiwon99 <102844803+baekjiwon99@users.noreply.github.com>
2023-06-07 21:40:29 +0900

    Update UBERStudent20190967.java

--- HW3/UBERStudent20190967.java
+++ HW3/UBERStudent20190967.java
@@ -24,47 +24,48 @@ public final class UBERStudent20190967 {
             .appName("UBERStudent20190967")
             .getOrCreate();
 
-        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+       JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
 
-        JavaPairRDD<String, String> words = lines.mapToPair(new PairFunction<String, String, String>() {
-            public Tuple2<String, String> call(String s) {
-		String [] splited = s.split(",");
-		String date = splited[1];
-
-		SimpleDateFormat transFormat = new SimpleDateFormat("MM/dd/yyyy");
-                  String dayOfWeek = "";
-          
-                  try {
-                    Date dateObj = transFormat.parse(date);
+		PairFunction<String, String, String> fmf = new PairFunction<String, String, String>() {
+			public Tuple2<String,String> call(String s){
 				
-                    Calendar cal = Calendar.getInstance() ;
-                    cal.setTime(dateObj);
-				     
-                    int dayNum = cal.get(Calendar.DAY_OF_WEEK) ;
-					
-                    dayOfWeek = parseToString(dayNum);
-                  } catch (Exception e) {}
-                    String key = splited[0] + "," + dayOfWeek;
-                    String value = splited[3] + "," + splited[2];
-
-                    return new Tuple2(key, value);
-                   }
-        });
-
-        JavaPairRDD<String, String> counts = words.reduceByKey(new Function2<String, String, String>() {
-            public String call(String val1, String val2) {
-            
-                String[] val1_splt = val1.split(",");
-                String[] val2_splt = val2.split(",");
-
-                int trips = Integer.parseInt(val1_splt[0]) + Integer.parseInt(val2_splt[0]);
-                int vehicles = Integer.parseInt(val1_splt[1]) + Integer.parseInt(val2_splt[1]);
-
-                return trips + "," + vehicles;
-            }
-        });
-
-        counts.saveAsTextFile(args[1]);
-        spark.stop();
-    }
+				String[] str = s.split(",");
+				String val1 = str[0] + "," + returnDay(str[1]);
+				String val2 =  str[3] + "," + str[2];
+				return new Tuple2(val1, val2);
+			}
+		};
+		JavaPairRDD<String, String> pair = lines.mapToPair(fmf);
+		
+		Function2<String, String, String> f2 = new Function2<String, String, String>(){
+			public String call(String x, String y){
+				String[] word1 = x.split(",");
+				String[] word2 = y.split(",");
+				int num1 = Integer.parseInt(word1[0]) + Integer.parseInt(word2[0]);
+				int num2 = Integer.parseInt(word1[1]) + Integer.parseInt(word2[1]); 
+				String value = num1 + "," + num2;
+				return value;
+			}
+		};
+		JavaPairRDD<String, String> counts = pair.reduceByKey(f2);
+		counts.saveAsTextFile(args[1]);
+		spark.stop();
+	}
+	public static String returnDay(String date){ 
+		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy"); 
+		String[] week = {"SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT"}; 
+		Calendar cal = Calendar.getInstance(); 
+		Date getDate; 
+		try { 
+			getDate = format.parse(date); 
+			cal.setTime(getDate); 
+			int i = cal.get(Calendar.DAY_OF_WEEK) - 1; 
+			return week[i];
+		} catch (ParseException e) { 
+			e.printStackTrace(); 
+		} catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+		return "0";
+	}
 }
