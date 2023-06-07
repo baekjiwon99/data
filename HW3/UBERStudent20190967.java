11cc4cc24555defd3dbacd00e7d4cacda357fce6
baekjiwon99 <102844803+baekjiwon99@users.noreply.github.com>
2023-06-07 21:56:22 +0900

    Update UBERStudent20190967.java

--- HW3/UBERStudent20190967.java
+++ HW3/UBERStudent20190967.java
@@ -4,11 +4,17 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.*;
 import java.io.Serializable;
-import java.util.List;
-import java.util.regex.Pattern;
 import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import java.util.regex.Pattern;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 
 public final class UBERStudent20190967 {
 
@@ -24,48 +30,55 @@ public final class UBERStudent20190967 {
             .appName("UBERStudent20190967")
             .getOrCreate();
 
-       JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
 
-		PairFunction<String, String, String> fmf = new PairFunction<String, String, String>() {
-			public Tuple2<String,String> call(String s){
-				
-				String[] str = s.split(",");
-				String val1 = str[0] + "," + returnDay(str[1]);
-				String val2 =  str[3] + "," + str[2];
-				return new Tuple2(val1, val2);
-			}
-		};
-		JavaPairRDD<String, String> pair = lines.mapToPair(fmf);
+        PairFunction<String, String, String> pf = new PairFunction<String, String, String>() {
+            public Tuple2<String, String> call(String s) {
+		String[] values = s.split(",");
+            	StringTokenizer itr = new StringTokenizer(values[1], "/");
+		int year = 0;
+		int month = 0;
+		String date = "";
+		int day = 0;
 		
-		Function2<String, String, String> f2 = new Function2<String, String, String>(){
-			public String call(String x, String y){
-				String[] word1 = x.split(",");
-				String[] word2 = y.split(",");
-				int num1 = Integer.parseInt(word1[0]) + Integer.parseInt(word2[0]);
-				int num2 = Integer.parseInt(word1[1]) + Integer.parseInt(word2[1]); 
-				String value = num1 + "," + num2;
-				return value;
-			}
-		};
-		JavaPairRDD<String, String> counts = pair.reduceByKey(f2);
-		counts.saveAsTextFile(args[1]);
-		spark.stop();
-	}
-	public static String returnDay(String date){ 
-		SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy"); 
-		String[] week = {"SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT"}; 
-		Calendar cal = Calendar.getInstance(); 
-		Date getDate; 
-		try { 
-			getDate = format.parse(date); 
-			cal.setTime(getDate); 
-			int i = cal.get(Calendar.DAY_OF_WEEK) - 1; 
-			return week[i];
-		} catch (ParseException e) { 
-			e.printStackTrace(); 
-		} catch (Exception e) { 
-			e.printStackTrace(); 
-		} 
-		return "0";
-	}
+            	while (itr.hasMoreTokens()) {
+            		month = Integer.parseInt(itr.nextToken());
+            		day = Integer.parseInt(itr.nextToken());
+            		year = Integer.parseInt(itr.nextToken());
+
+            	}
+		    
+            	LocalDate localDate = LocalDate.of(year, month, day);
+		DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+		date = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+		    
+		if (date.equals("THU")) date = "THR";
+		
+		String key = values[0] + "," + date;
+		String value = values[3] + "," + values[2];
+	
+                return new Tuple2(key, value);
+            }
+        };
+	    
+        JavaPairRDD<String, String> pair = lines.mapToPair(pf);
+	    
+        Function2<String, String, String> f2 = new Function2<String, String, String>() {
+            public String call(String x, String y) {
+            	String[] v1 = x.split(",");
+            	String[] v2 = y.split(",");
+            	
+            	int trips = Integer.parseInt(v1[0]) + Integer.parseInt(v2[0]);
+            	int vehicles = Integer.parseInt(v1[1]) + Integer.parseInt(v2[1]);
+            
+                return trips + "," + vehicles;
+            }
+        };
+        JavaPairRDD<String, String> reduce = pair.reduceByKey(f2);
+        
+        JavaRDD<String> result = reduce.map(x -> x._1 + " " + x._2);
+
+        result.saveAsTextFile(args[1]);
+        spark.stop();
+    }
 }
